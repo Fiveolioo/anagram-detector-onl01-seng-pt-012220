924fe30da498712ce34d192984c47d7db9fd5710
Richard Cho <96richardohc@gmail.com>
2020-02-26 22:23:04 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,13 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+  
+  def initialize(word)
+    @word=word
+  end
+  
+  def match(anagrams)
+    anagrams.find_all do |anagram|
+    end
+  end
+end
